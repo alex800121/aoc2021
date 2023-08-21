f03f40b76c2b0a8033c4d702b3dbe48a57de32f8
alex800121 <alex800121@hotmail.com>
2023-08-22 02:27:59 +0800

    on day15

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,4 +1,62 @@
 module Day14 where
 
+import Data.List.Split (divvy, splitOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.MultiSet (MultiSet)
+import qualified Data.MultiSet as MultiSet
+import Data.Char (isSpace)
+
+type Formula = Map String (MultiSet String)
+
+type Polymer = MultiSet String
+
+type Molecules = MultiSet Char
+
+parseInput :: String -> (Polymer, Formula, Char, Char)
+parseInput s = (polymer, formula, head x, last x)
+  where
+    (x : y : _) = splitOn "\n\n" s
+    polymer = MultiSet.fromList $ divvy 2 1 x
+    formula = Map.fromList $ map f $ lines y
+    f n = (input, output)
+      where
+        (a : b : _) = splitOn " -> " n
+        input = a
+        output = MultiSet.fromList [head a : b, head b : tail a]
+
+step :: Formula -> Polymer -> Polymer
+step f = MultiSet.unionsMap (f Map.!)
+
+calc :: Polymer -> Molecules
+calc = MultiSet.unionsMap MultiSet.fromList
+
+ansA :: Molecules -> Int
+ansA m = (maxM - minM) `div` 2
+  where
+    m' = map snd $ MultiSet.toOccurList m
+    maxM = maximum m'
+    minM = minimum m'
+
 day14 :: IO ()
-day14 = return ()
+day14 = do
+  input <- readFile "input/input14.txt"
+  let (polymer, formula, start, end) = parseInput input
+  putStrLn
+    . ("day14a: " ++)
+    . show
+    . ansA
+    . MultiSet.insert end
+    . MultiSet.insert start
+    . calc
+    . (!! 10)
+    $ iterate (step formula) polymer
+  putStrLn
+    . ("day14b: " ++)
+    . show
+    . ansA
+    . MultiSet.insert end
+    . MultiSet.insert start
+    . calc
+    . (!! 40)
+    $ iterate (step formula) polymer
--- src/Day15.hs
+++ src/Day15.hs
@@ -1,4 +1,89 @@
 module Day15 where
 
+import Data.Char (digitToInt)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Debug.Trace
+import MyLib (drawGraph, drawMap, sqrtCeiling)
+
+type Index = (Int, Int)
+
+type Risk = Int
+
+type Weight = Int
+
+type Candidate = (Risk, Index, Set Index)
+
+type Candidate' = (Weight, Risk, Index, Set Index)
+
+type Chitons = Map Index Int
+
+type Queue = Set Candidate
+
+type Queue' = Set Candidate'
+
+type Cache = Set (Index, Set Index)
+
+adjacent = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+dijkstra :: Chitons -> Index -> Queue -> Cache -> Int
+dijkstra chitons end q cache
+  -- \| trace (show (length q)) False = undefined
+  | start == end = nextRisk
+  | (start, path) `Set.member` cache = dijkstra chitons end q' cache
+  | otherwise = dijkstra chitons end q'' cache'
+  where
+    (current@(risk, start, path), q') = Set.deleteFindMin q
+    nextRisk = risk + chitons Map.! start
+    nexts =
+      Set.fromList $
+        mapMaybe
+          ( \(x, y) ->
+              let next = (fst start + x, snd start + y)
+               in if Map.member next chitons && Set.notMember next path
+                    then Just (nextRisk, next, Set.insert next path)
+                    else Nothing
+          )
+          adjacent
+    q'' = Set.union nexts q'
+    cache' = Set.insert (start, path) cache
+
+-- aStar :: Chitons -> Index -> Queue' -> Cache -> Int
+-- aStar chitons end q cache
+--   | trace (show (length q, start)) False = undefined
+--   | start == end = nextRisk
+--   | (start, path) `Map.member` cache = aStar chitons end q' cache
+--   | otherwise = aStar chitons end q'' cache'
+--   where
+--     (current@(weight, risk, start, path), q') = Set.deleteFindMin q
+--     nextRisk = risk + chitons Map.! start
+--     nextWeight = nextRisk + (2 * sqrtCeiling (abs (fst end - fst start) ^ 2 + abs (snd end - snd start) ^ 2))
+--     nexts =
+--       Set.fromList $
+--         mapMaybe
+--           ( \(x, y) ->
+--               let next = (fst start + x, snd start + y)
+--                in if Map.member next chitons && Set.notMember next path
+--                     then Just (nextWeight, nextRisk, next, Set.insert next path)
+--                     else Nothing
+--           )
+--           adjacent
+--     q'' = Set.union nexts q'
+--     cache' = Map.insertWith min (start, path) risk cache
+
 day15 :: IO ()
-day15 = return ()
+day15 = do
+  input <- drawMap (Just . digitToInt) . lines <$> readFile "input/input15.txt"
+  -- input <- drawMap (Just . digitToInt) . lines <$> readFile "input/test15.txt"
+  let keys = Map.keys input
+      start = minimum keys
+      end = maximum keys
+      initRisk = negate (input Map.! start)
+      initWeight = initRisk + abs (fst end - fst start) + abs (snd end - snd start)
+  print end
+  -- print $ dijkstra input end (Set.singleton (negate (input Map.! start), start, Set.empty)) Set.empty
+
+-- print $ aStar input end (Set.singleton (initWeight, initRisk, start, Set.empty)) Map.empty
