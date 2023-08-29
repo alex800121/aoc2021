64da9a417e318cdad31834b36914244d31f1fbc9
alex800121 <alex800121@hotmail.com>
2023-08-29 08:36:33 +0800

    on day20

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,7 +1,7 @@
 module Day19 where
 
 import Control.Applicative (Alternative (empty))
-import Data.List (intersect, nub, transpose)
+import Data.List (intersect, nub, transpose, insert)
 import Data.List.Split (splitOn)
 import Data.Maybe (listToMaybe)
 import Data.Char (isDigit)
@@ -10,15 +10,16 @@ import qualified Data.Set as Set
 import Control.Monad (guard)
 import Debug.Trace (trace)
 import Data.Either (partitionEithers)
+import Data.Bifunctor (bimap)
 
 type Index = (Int, Int, Int)
 
 rotate :: Index -> Index
 rotate (x, y, z) = (y, z, x)
 
-orientations :: Index -> [Index]
+orientations :: Set Index -> [(Set Index, Index -> Index)]
 orientations s =
-  [ g n . f3 . f2 $ s
+  [ (Set.map (g n . f3 . f2) s, g n . f3 . f2)
     | n <- [0 .. 3],
       let g n x = iterate (\(a, b, c) -> (a, c, -b)) x !! n,
       f2 <- [id, rotate, rotate . rotate],
@@ -28,36 +29,43 @@ orientations s =
 sub :: Index -> Index -> Index
 sub (a, b, c) (d, e, f) = (a - d, b - e, c - f)
 
-parseInput :: String -> (Int, [Index])
-parseInput s = (n, i)
+parseInput :: String -> (Int, Set Index)
+parseInput s = (n, Set.fromList i)
   where
     s' = lines s
     n = read $ filter isDigit $ head s'
     i = map ((\(x : y : z : _) -> (x, y, z)) . map read . splitOn ",") $ tail s'
 
-overlap :: [Index] -> [Index] -> Either [Index] [Index]
+overlap :: Set Index -> Set Index -> Either (Set Index) (Index, Set Index)
 overlap ref x = maybe (Left x) Right $ listToMaybe $ do
-  r <- ref
-  oriented <- traverse orientations x
-  picked <- oriented
+  r <- Set.toList ref
+  (oriented, transformer) <- orientations x
+  picked <- Set.toList oriented
   let diff = picked `sub` r
-  let moved = map (`sub` diff) oriented
+  let moved = Set.map (`sub` diff) oriented
   -- return (ref `intersect` moved)
-  guard $ length (ref `intersect` moved) >= 12 
-  return moved
+  guard $ length (Set.intersection ref moved) >= 12 
+  return ((0, 0, 0) `sub` diff, moved)
 
-day19a :: [[Index]] -> [[Index]] -> Set Index -> Set Index
-day19a x y acc | trace (show (length x, length y, length acc)) False = undefined
-day19a [] (y : ys) acc = day19a [y] ys acc
-day19a xs [] acc = acc <> Set.fromList (concat xs)
-day19a (x : xs) ys acc = day19a (xs <> ysRight) ysLeft (acc <> Set.fromList x)
+day19a :: [(Index, Set Index)] -> [Set Index] -> ([Index], Set Index) -> ([Index], Set Index)
+-- day19a x y acc | trace (show (length x, length y, length $ snd acc)) False = undefined
+day19a [] (y : ys) acc = day19a [((0, 0, 0), y)] ys acc
+day19a xs [] acc = bimap (map fst xs <>) (Set.unions (map snd xs) <>) acc
+day19a (x : xs) ys acc = day19a (xs <> ysRight) ysLeft (bimap (insert (fst x)) (snd x <>) acc)
   where
-    (ysLeft, ysRight) = partitionEithers (map (overlap x) ys)
+    (ysLeft, ysRight) = partitionEithers (map (overlap (snd x)) ys)
+
+day19b :: [Index] -> Int
+day19b s = go s 0
+  where
+    go [] m = m
+    go (x : xs) m = go xs $ maximum $ m : map ((\(x, y, z) -> abs x + abs y + abs z) . sub x) xs
 
 day19 :: IO ()
 day19 = do
   input <- map parseInput . splitOn "\n\n" <$> readFile "input/input19.txt"
   -- input <- map parseInput . splitOn "\n\n" <$> readFile "input/test19.txt"
   let input' = map snd input
-  print $ traverse orientations [(1, 2, 3), (4, 5, 6)]
-  -- print $ length $ day19a [] input' Set.empty
+      ans = day19a [] input' ([], Set.empty)
+  putStrLn $ ("day19a: " ++) $ show $ length $ snd ans
+  putStrLn $ ("day19b: " ++) $ show $ day19b $ fst ans
