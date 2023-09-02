b7c6ef33e77027139c1a52c7481c074324847602
alex800121 <alex800121@hotmail.com>
2023-09-02 13:21:56 +0800

    on day23

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,4 +1,36 @@
 module Day22 where
 
+import Data.List.Split (splitOn, splitOneOf)
+import MyLib
+import Data.List (uncons, foldl')
+import Data.Char (isNumber)
+import Data.Maybe (mapMaybe)
+
+type S3 = S (S (S Z))
+type V3 = Vec S3
+type Range = (Int, Int)
+s3 = SS (SS (SS SZ))
+
+parseInput :: String -> (Bool, V3 Range)
+parseInput s = (b', v)
+  where
+    Just (b, s') = uncons $ words s
+    b' = b == "on"
+    t = map (read @Int) $ filter (any isNumber) $ splitOneOf "=,." $ head s'
+    v = toVec s3 [(head t, t !! 1 + 1), (t !! 2, t !! 3 + 1), (t !! 4, t !! 5 + 1)]
+
+readIns :: [(Bool, V3 Range)] -> [V3 Range]
+readIns = foldl' f []
+  where
+    f acc (True, v) = jointEucVecs' acc v
+    f acc (False, v) = subtractEucVecs' v acc
+
+calcOn :: V3 Range -> Int
+calcOn = foldl' (\acc (x, y) -> acc * (y - x)) 1
+
 day22 :: IO ()
-day22 = return ()
+day22 = do
+  input <- map parseInput . lines <$> readFile "input/input22.txt"
+  let ans = readIns input
+  putStrLn $ ("day22a: " ++) $ show $ sum $ mapMaybe (fmap calcOn . overlapEucVec (toVec s3 (replicate 3 (-50, 51)))) ans
+  putStrLn $ ("day22b: " ++) $ show $ sum $ map calcOn ans
--- src/Day23.hs
+++ src/Day23.hs
@@ -1,4 +1,38 @@
 module Day23 where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.PQueue.Prio.Min (MinPQueue)
+import qualified Data.PQueue.Prio.Min as Q
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+type Index = (Int, Int)
+
+type Risk = Int
+
+data Pods = A | B | C | D deriving (Show, Eq, Ord)
+
+data Space
+  = Corridor {getPod :: Maybe Pods}
+  | Dest {dest :: Pods, getPod :: Maybe Pods}
+  | Space {getPod :: Maybe Pods}
+  deriving (Show, Eq, Ord)
+
+candidates :: Map Index Space -> Index -> [Map Index Space]
+candidates m i = case m Map.!? i of
+  Just (Corridor (Just p)) -> undefined
+  Just (Dest p (Just p'))
+    | p /= p' || any cond [fmap (+ 1) i, fmap (subtract 1) i] ->
+        undefined
+  _ -> []
+  where
+    cond b = case m Map.!? b of
+      Just (Dest c (Just c')) -> c /= c'
+      _ -> False
+
+adjacent :: [Index]
+adjacent = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
 day23 :: IO ()
 day23 = return ()
