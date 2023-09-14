445602cb3d872c312f62af287761efb3fe9c3b13
alex800121 <alex800121@hotmail.com>
2023-09-15 07:09:35 +0800

    on day24

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Day24 where
+
+import Control.Applicative ((<|>))
+import Data.Maybe (mapMaybe)
+import MyLib (Parser, signedInteger)
+import Text.Megaparsec (choice, eof, parseMaybe, single)
+import Text.Megaparsec.Char (space, string)
+import Data.SBV
 
 data Instruction a b
   = Inp a
@@ -7,9 +16,34 @@ data Instruction a b
   | Div a b
   | Mod a b
   | Eql a b
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq, Ord, Functor)
 
-data Vars a = R {_w :: a, _x :: a, _y :: a, _z :: a} deriving (Show, Eq, Ord)
+data Vars a = R {_w :: !a, _x :: !a, _y :: !a, _z :: !a} deriving (Show, Eq, Ord, Functor)
+
+data Reg = W | X | Y | Z deriving (Show, Eq, Ord)
+
+inputParser :: Parser (Instruction Reg (Either Reg Int))
+inputParser =
+  choice
+    [ string "inp " >> (Inp <$> reg),
+      string "add " >> (Add <$> reg <*> intOrReg),
+      string "mul " >> (Mul <$> reg <*> intOrReg),
+      string "div " >> (Div <$> reg <*> intOrReg),
+      string "mod " >> (Mod <$> reg <*> intOrReg),
+      string "eql " >> (Eql <$> reg <*> intOrReg)
+    ]
+  where
+    reg =
+      choice
+        [ single 'w' >> return W,
+          single 'x' >> return X,
+          single 'y' >> return Y,
+          single 'z' >> return Z
+        ]
+        <* space
+    intOrReg = (Left <$> reg) <|> (Right <$> signedInteger)
 
 day24 :: IO ()
-day24 = return ()
+day24 = do
+  input <- mapMaybe (parseMaybe inputParser) . lines <$> readFile "input/input24.txt"
+  print input
