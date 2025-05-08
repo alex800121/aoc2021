4e55c6586636b39e8b54a479be88032c89a8b0e4
alex800121 <alex800121@hotmail.com>
2025-05-08 21:51:02 +0300

    fast day25

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,59 +1,76 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Day25 where
 
-import Data.Bifunctor (bimap)
-import Data.Either (isLeft)
-import Data.Either qualified as Set
-import Data.Map (fromSet, keysSet, partition, union)
-import Data.Set (Set)
-import Data.Set qualified as Set
-import MyLib (drawGraph, drawMap, firstRepeat)
-import Paths_AOC2021
+import Data.Bits (Bits (..))
+import Data.Vector.Storable (Vector)
+import Data.Vector.Storable qualified as V
+import Data.Vector.Strict qualified as S
+import Data.WideWord (Word256)
+import MyLib (drawArray)
+import Paths_AOC2021 (getDataDir)
 
 type Index = (Int, Int)
 
-data SC = SC {_height :: Int, _width :: Int, _east :: Set Index, _south :: Set Index}
-  deriving (Show, Eq, Ord)
-
-run :: (SC -> Int) -> ((Int -> Int -> Int) -> Int -> Index -> Index) -> (SC -> Set Index) -> (SC -> Set Index) -> (Set Index -> SC -> SC) -> SC -> SC
-run f1 f2 f3 f4 f5 sc = f5 e' sc
+inputParser i = (w, e, s)
   where
-    w = f1 sc
-    f x = let a = f2 (+) w x in if a `Set.member` f4 sc || a `Set.member` f3 sc then x else a
-    e' = Set.map f $ f3 sc
+    ss = lines i
+    e = V.fromList $ map (f '>' 0 (0 :: Word256)) ss
+    s = V.fromList $ map (f 'v' 0 (0 :: Word256)) ss
+    w = length (head ss)
+    f a i c [] = c
+    f a i c (b : bs) = f a (succ i) (if a == b then c `setBit` (w - 1 - i) else c) bs
 
--- f x = let a = f2 (+) w x in if a `Set.member` f4 sc then Left x else Right a
--- (l, r) =
---   Set.fold
---     ( \x (accx, accy) -> case f x of
---         Left x -> (Set.insert x accx, accy)
---         Right x -> (accx, Set.insert x accy)
---     )
---     (Set.empty, Set.empty)
---     $ f3 sc
--- g (x, y)
---   | Set.null x = y
---   | otherwise = g (x', yr `Set.union` x)
---   where
---     (yl, yr) = Set.partition (`Set.member` x) y
---     x' = Set.map (f2 (-) w) yl
--- e' = g (l, r)
+east w = V.map (\x -> (if x `testBit` 0 then (`setBit` (w - 1)) else id) (x `shiftR` 1))
 
-runEast = run _width (\f w (x, y) -> (f x 1 `mod` w, y)) _east _south (\x sc -> sc {_east = x})
+west w = V.map (\x -> (if x `testBit` (w - 1) then (`clearBit` w) . (`setBit` 0) else id) (x `shiftL` 1))
 
-runSouth = run _height (\f h (x, y) -> (x, f y 1 `mod` h)) _south _east (\x sc -> sc {_south = x})
+south v = V.cons (V.last v) (V.init v)
 
-printSC :: SC -> String
-printSC sc = unlines $ drawGraph (\case Nothing -> '.'; Just True -> '>'; Just False -> 'v') $ union (fromSet (const True) (_east sc)) (fromSet (const False) (_south sc))
+north v = V.snoc (V.tail v) (V.head v)
+
+bitMask a b = a .&. complement b
+
+moveEast (w, e, s) = (w, e', s)
+  where
+    proposed = east w e
+    masked = V.zipWith bitMask proposed (V.zipWith (.|.) e s)
+    masked' = west w masked
+    e' = V.zipWith3 (\a b c -> bitMask (a .|. b) c) e masked masked'
+
+moveSouth (w, e, s) = (w, e, s')
+  where
+    proposed = south s
+    masked = V.zipWith bitMask proposed (V.zipWith (.|.) e s)
+    masked' = north masked
+    s' = V.zipWith3 (\a b c -> bitMask (a .|. b) c) s masked masked'
+
+move :: (Int, Vector Word256, Vector Word256) -> (Int, Vector Word256, Vector Word256)
+move = moveSouth . moveEast
+
+printer (w, e, s) = unlines (zipWith (zipWith g) ve vs)
+  where
+    g b c
+      | b = '>'
+      | c = 'v'
+      | otherwise = '.'
+    f i x
+      | i < 0 = []
+      | otherwise = (x `testBit` i) : f (pred i) x
+    ve = map (f (w - 1)) $ V.toList e
+    vs = map (f (w - 1)) $ V.toList s
+
+stable f x = go 1 x
+  where
+    go n x
+      | x == x' = n
+      | otherwise = go (succ n) x'
+      where
+        x' = f x
 
 day25 :: IO ()
 day25 = do
-  -- input <- lines <$> readFile "input/test25.txt"
-  input <- lines <$> (getDataDir >>= readFile . (++ "/input/input25.txt"))
-  let h = length input
-      w = length (head input)
-      (e, s) = bimap keysSet keysSet $ partition id $ drawMap (\case '>' -> Just True; 'v' -> Just False; _ -> Nothing) input
-      sc = SC h w e s
-  putStrLn $ ("day25a: " ++) $ show $ fmap fst $ firstRepeat $ iterate (runSouth . runEast) sc
+  input <- inputParser <$> (getDataDir >>= readFile . (++ "/input/input25.txt"))
+  putStrLn
+    . ("day25a: " ++)
+    . show
+    $ stable move input
   putStrLn "Merry Christmas!!!"
