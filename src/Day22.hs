747bf2f577adee3e87d432a8954635ee7f175d10
alex800121 <alex800121@hotmail.com>
2025-05-08 11:36:36 +0300

    fast day22

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,19 +1,27 @@
 module Day22 where
 
+import Control.Parallel (par)
 import Data.Char (isNumber)
-import Data.List (foldl', uncons)
-import Data.List.Split (splitOn, splitOneOf)
+import Data.Function (on)
+import Data.List (foldl', sortBy, uncons)
 import Data.Maybe (mapMaybe)
-import MyLib
+import Data.MultiSet (MultiSet)
+import Data.MultiSet qualified as MS
 import Paths_AOC2021
-
-type S3 = S (S (S Z))
-
-type V3 = Vec S3
+import Data.List.Split (splitOneOf)
 
 type Range = (Int, Int)
 
-s3 = SS (SS (SS SZ))
+type V3 a = (a, a, a)
+
+overlapEucVec x@((a0, a1), (b0, b1), (c0, c1)) y@((d0, d1), (e0, e1), (f0, f1))
+  | g1 > g0 && h1 > h0 && i1 > i0 = Just ans
+  | otherwise = Nothing
+  where
+    g@(g0, g1) = (max a0 d0, min a1 d1)
+    h@(h0, h1) = (max b0 e0, min b1 e1)
+    i@(i0, i1) = (max c0 f0, min c1 f1)
+    ans = (g, h, i)
 
 parseInput :: String -> (Bool, V3 Range)
 parseInput s = (b', v)
@@ -21,20 +29,31 @@ parseInput s = (b', v)
     Just (b, s') = uncons $ words s
     b' = b == "on"
     t = map (read @Int) $ filter (any isNumber) $ splitOneOf "=,." $ head s'
-    v = toVec s3 [(head t, t !! 1 + 1), (t !! 2, t !! 3 + 1), (t !! 4, t !! 5 + 1)]
+    v = ((head t, t !! 1 + 1), (t !! 2, t !! 3 + 1), (t !! 4, t !! 5 + 1))
 
-readIns :: [(Bool, V3 Range)] -> [V3 Range]
-readIns = foldl' f []
+readIns :: [(Bool, V3 Range)] -> (MultiSet (V3 Range), MultiSet (V3 Range))
+readIns = foldl' f (MS.empty, MS.empty)
   where
-    f acc (True, v) = jointEucVecs' acc v
-    f acc (False, v) = subtractEucVecs' v acc
+    f (on, off) (b, v) = (on', off')
+      where
+        on' = (if b then MS.insert v else id) (MS.mapMaybe (overlapEucVec v) off <> on)
+        off' = par on' MS.mapMaybe (overlapEucVec v) on <> off
 
 calcOn :: V3 Range -> Int
-calcOn = foldl' (\acc (x, y) -> acc * (y - x)) 1
+calcOn ((x0, x1), (y0, y1), (z0, z1)) = (x1 - x0) * (y1 - y0) * (z1 - z0)
 
 day22 :: IO ()
 day22 = do
   input <- map parseInput . lines <$> (getDataDir >>= readFile . (++ "/input/input22.txt"))
   let ans = readIns input
-  putStrLn $ ("day22a: " ++) $ show $ sum $ mapMaybe (fmap calcOn . overlapEucVec (toVec s3 (replicate 3 (-50, 51)))) ans
-  putStrLn $ ("day22b: " ++) $ show $ sum $ map calcOn ans
+      area = ((-50, 50), (-50, 50), (-50, 50))
+      ansA = par ansB uncurry ((-) `on` (sum . MS.mapMaybe (fmap calcOn . overlapEucVec area))) ans
+      ansB = uncurry ((-) `on` (sum . MS.map calcOn)) ans
+  putStrLn
+    . ("day22a: " ++)
+    . show
+    $ ansA
+  putStrLn
+    . ("day22b: " ++)
+    . show
+    $ ansB
