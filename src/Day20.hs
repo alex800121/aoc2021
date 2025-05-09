9023fc9bdb2a16e140c7a2bdc1a3686f56f85732
alex800121 <alex800121@hotmail.com>
2025-05-09 18:49:04 +0300

    faster day20

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,79 +1,82 @@
-{-# LANGUAGE TupleSections #-}
-
 module Day20 where
 
-import Control.Monad (forM)
-import Control.Monad.Trans.State.Strict
-import Data.Array.IArray (Array, IArray (bounds), array, elems, listArray, (!))
-import Data.Bifunctor (bimap)
-import Data.Char (isSpace)
-import Data.List (foldl')
-import Data.List.Split (splitOn)
-import Data.Map (Map)
-import Data.Map qualified as Map
-import Data.Maybe (fromMaybe)
-import MyLib (drawMap)
+import Control.Parallel.Strategies (parMap, rpar)
+import Data.Array.IArray qualified as A
+import Data.Array.Unboxed (Array, UArray)
+import Data.Bits (Bits (..), FiniteBits (..))
+import Data.List.Split (divvy, splitOn)
+import Data.Vector.Storable (Vector)
+import Data.Vector.Storable qualified as V
+import Data.WideWord
+import MyLib (drawArray)
 import Paths_AOC2021
 
-type Rule = Array Int Bool
+type Trench = (Int, [Word256])
 
-type Gen = Int
+type Rule = Vector Bool
 
-type Index = (Int, Int)
-
-type Bound = (Index, Index)
-
-type Mem = Map (Gen, Index) Bool
-
-type Trench = (Bool, Array Index Bool)
-
-nextTrench :: Rule -> Trench -> Trench
-nextTrench rule (def, trench) = (not def, trench')
+inputParser s = (V.fromList (map (== '#') $ filter (/= '\n') x), (width, a))
   where
-    ((minX, minY), (maxX, maxY)) = bounds trench
-    newBound@((minX', minY'), (maxX', maxY')) = ((minX - 1, minY - 1), (maxX + 1, maxY + 1))
-    f x y
-      | x < minX || x > maxX || y < minY || y > maxY = def
-      | otherwise = trench ! (x, y)
-    g x y = rule ! foldl' (\acc z -> (acc * 2) + if z then 1 else 0) 0 (map (uncurry f . bimap (+ x) (+ y)) box)
-    trench' = array newBound [((x, y), g x y) | x <- [minX' .. maxX'], y <- [minY' .. maxY']]
+    [x, y] = splitOn "\n\n" s
+    ys = lines y
+    width = length (head ys)
+    a = map (f (0 :: Word256) (width - 1)) ys
+    f acc _ [] = acc
+    f acc i ('#' : cs) = f (acc `setBit` i) (pred i) cs
+    f acc i (_ : cs) = f acc (pred i) cs
 
-box :: [Index]
-box = [(x, y) | y <- [-1 .. 1], x <- [-1 .. 1]]
-
-getGenIndex :: Bound -> Rule -> (Gen, Index) -> State Mem Bool
-getGenIndex ((minX, maxX), (minY, maxY)) rule (gen, index@(x, y))
-  | x < minX - gen || x > maxX + gen || y < minY - gen || y > maxY + gen = return (odd gen)
-getGenIndex bound rule (gen, index) =
-  get >>= \mem -> case mem Map.!? (gen, index) of
-    Just x -> return x
-    Nothing | gen <= 0 -> return False
-    Nothing -> do
-      b <- forM box $ \(x, y) -> getGenIndex bound rule (gen - 1, bimap (+ x) (+ y) index)
-      let b' = foldl' (\acc x -> (acc * 2) + if x then 1 else 0) 0 b
-          ans = rule ! b'
-      modify (Map.insert (gen, index) ans)
-      return ans
-
-printArray :: (a -> b) -> Array Index a -> [[b]]
-printArray f arr = g minX minY id
+step :: Int -> Rule -> Trench -> Trench
+step initWidth rule (gen, v0) = (nextGen, vF)
   where
-    ((minX, minY), (maxX, maxY)) = bounds arr
-    g x y acc
-      | y > maxY = []
-      | x > maxX = acc [] : g minX (y + 1) id
-      | otherwise = g (x + 1) y (acc . (f (arr ! (x, y)) :))
+    nextGen = succ gen
+    nextWidth = initWidth + nextGen * 2
+    ones8 = [replicate nextWidth 7, replicate nextWidth 7]
+    zeros8 = [replicate nextWidth 0, replicate nextWidth 0]
+    oneBits = (3 `setBit` nextWidth) `setBit` (nextWidth + 1)
+    vRef
+      | gen `testBit` 0 = concat [ones8, map (calc8Bits nextWidth [] . (.|. oneBits) . (`shiftL` 2)) v0, ones8]
+      | otherwise = concat [zeros8, map (calc8Bits nextWidth [] . (`shiftL` 2)) v0, zeros8]
+    f [x0, x1, x2] = g 0 (nextWidth - 1) (zipWith3 (\a b c -> (a `shiftL` 6) .|. (b `shiftL` 3) .|. c) x0 x1 x2)
+    vF = parMap rpar f $ divvy 3 1 vRef
+    g acc i (x : xs) = g acc' (pred i) xs
+      where
+        acc' = if rule V.! fromIntegral x then acc `setBit` i else acc
+    g acc _ _ = acc
+
+printer initWidth (g, v) = unlines $ parMap rpar (go "" w) v
+  where
+    w = initWidth + 2 * g
+    go acc i x
+      | i <= 0 = acc
+      | otherwise = go (c : acc) (pred i) (x `shiftR` 1)
+      where
+        c = if x `testBit` 0 then '#' else '.'
+
+solve initWidth (g, v) = sum (parMap rpar (go 0 w) v)
+  where
+    w = initWidth + 2 * g
+    go acc i x
+      | i <= 0 = acc
+      | otherwise = go acc' (pred i) (x `shiftR` 1)
+      where
+        acc' = if x `testBit` 0 then acc + 1 else acc
+
+calc8Bits w acc x
+  | w <= 0 = acc
+  | otherwise = calc8Bits (pred w) ((x .&. 7) : acc) (x `shiftR` 1)
 
 day20 :: IO ()
 day20 = do
-  input <- splitOn "\n\n" <$> (getDataDir >>= readFile . (++ "/input/input20.txt"))
-  let rule = listArray (0, 511) $ map (== '#') $ filter (not . isSpace) $ head input :: Array Int Bool
-      mem = Map.mapKeys (0,) $ drawMap (Just . (== '#')) $ lines (input !! 1)
-      (minX, minY) = minimum $ map snd $ Map.keys mem
-      (maxX, maxY) = maximum $ map snd $ Map.keys mem
-      arr = array ((minX, minY), (maxX, maxY)) $ Map.toList $ Map.mapKeys snd mem :: Array Index Bool
-      rangeN n = [(n, (x, y)) | x <- [minX - n .. maxX + n], y <- [minY - n .. maxY + n]]
-      rangeA = rangeN 2
-      rangeB = rangeN 50
-  putStrLn $ ("day20a: " ++) $ show $ length $ filter id $ elems $ snd $ (!! 2) $ iterate (nextTrench rule) (False, arr)
-  putStrLn $ ("day20b: " ++) $ show $ length $ filter id $ elems $ snd $ (!! 50) $ iterate (nextTrench rule) (False, arr)
+  (rule, (initWidth, pic)) <- inputParser <$> (getDataDir >>= readFile . (++ "/input/input20.txt"))
+  putStrLn
+    . ("day20b: " ++)
+    . show
+    . solve initWidth
+    $ (!! 2)
+    $ iterate (step initWidth rule) (0, pic)
+  putStrLn
+    . ("day20b: " ++)
+    . show
+    . solve initWidth
+    $ (!! 50)
+    $ iterate (step initWidth rule) (0, pic)
