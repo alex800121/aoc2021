006b1b78c9d7c0d21e63e57cfe35334268cc2373
alex800121 <alex800121@hotmail.com>
2025-05-10 07:14:54 +0300

    fast day5

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,29 +1,54 @@
 module Day5 where
 
+import Control.Monad.ST.Strict (ST, runST)
+import Data.Array.IArray qualified as A
+import Data.Array.Unboxed (Array)
 import Data.List.Split qualified as Split
-import Data.Map qualified as Map
+import Data.Map.Strict qualified as Map
+import Data.Vector.Unboxed qualified as V
+import Data.Vector.Unboxed.Mutable (STVector)
+import Data.Vector.Unboxed.Mutable qualified as M
+import Data.Word (Word8)
 import Paths_AOC2021
 
 type Index = (Int, Int)
 
 type Ends = (Index, Index)
 
-type Line = Map.Map Index Int
+straight ((sx, sy), (ex, ey)) = sy == ey || sx == ex
 
-drawLine :: Ends -> Line
-drawLine (start, end) = go acc start end
+drawLine :: STVector s Int -> Ends -> ST s ()
+drawLine v ((sx, sy), (ex, ey)) = mapM_ (f v) l
   where
-    x = signum (fst end - fst start)
-    y = signum (snd end - snd start)
-    acc = Map.empty
-    go acc start end
-      | start == end = Map.insertWith (+) end 1 acc
-      | otherwise = go (Map.insertWith (+) start 1 acc) (fst start + x, snd start + y) end
+    dx = signum (ex - sx)
+    dy = signum (ey - sy)
+    lx = [sx, sx + dx .. ex]
+    ly = [sy, sy + dy .. ey]
+    l = zipWith (\x y -> x * 1000 + y) lx ly
+    f v = M.modify v succ
+
+run ls = runST $ do
+  v <- M.replicate (1000 * 1000) 0
+  mapM_ (drawLine v) ls
+  M.foldl' (\acc x -> if x > 1 then succ acc else acc) 0 v
+
+inputParser =
+  map
+    ( (\(x : y : _) -> (x, y))
+        . map ((\(x : y : _) -> (x, y)) . map (read @Int) . Split.splitOn ",")
+        . Split.splitOn " -> "
+    )
+    . lines
 
 day5 :: IO ()
 day5 = do
-  input <- map ((\(x : y : _) -> (x, y)) . map ((\(x : y : _) -> (x, y)) . map (read @Int) . Split.splitOn ",") . Split.splitOn " -> ") . lines <$> (getDataDir >>= readFile . (++ "/input/input5.txt"))
-  let lineA = length $ Map.filter (> 1) $ Map.unionsWith (+) $ map drawLine $ filter (\((x1, y1), (x2, y2)) -> x1 == x2 || y1 == y2) input
-      lineB = length $ Map.filter (> 1) $ Map.unionsWith (+) $ map drawLine input
-  putStrLn $ ("day5a: " ++) $ show lineA
-  putStrLn $ ("day5b: " ++) $ show lineB
+  input <- inputParser <$> (getDataDir >>= readFile . (++ "/input/input5.txt"))
+  putStrLn
+    . ("day5a: " ++)
+    . show
+    . run
+    $ filter straight input
+  putStrLn
+    . ("day5b: " ++)
+    . show
+    $ run input
