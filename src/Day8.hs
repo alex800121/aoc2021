d4e62186b2a9fd22df75be5d71c76113dfa9616b
alex800121 <alex800121@hotmail.com>
2025-05-11 07:57:14 +0300

    faster day8

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,38 +1,47 @@
 module Day8 where
 
+import Data.Bifunctor (Bifunctor (first))
+import Data.Bits (Bits (..))
 import Data.Char (chr, ord)
+import Data.IntMap.Strict (IntMap)
+import Data.IntMap.Strict qualified as IM
 import Data.List (elemIndex, find, findIndex, foldl', permutations, sort)
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (catMaybes, fromJust)
 import MyLib (mapFirst)
-import Paths_AOC2021
+import Paths_AOC2021 (getDataDir)
 
-original :: Map [Int] Int
+original :: [([Int], Int)]
 original =
-  Map.fromList $
-    zip
-      [ [0, 1, 2, 4, 5, 6],
-        [2, 5],
-        [0, 2, 3, 4, 6],
-        [0, 2, 3, 5, 6],
-        [1, 2, 3, 5],
-        [0, 1, 3, 5, 6],
-        [0, 1, 3, 4, 5, 6],
-        [0, 2, 5],
-        [0, 1, 2, 3, 4, 5, 6],
-        [0, 1, 2, 3, 5, 6]
-      ]
-      [0 .. 9]
+  zip
+    [ [0, 1, 2, 4, 5, 6],
+      [2, 5],
+      [0, 2, 3, 4, 6],
+      [0, 2, 3, 5, 6],
+      [1, 2, 3, 5],
+      [0, 1, 3, 5, 6],
+      [0, 1, 3, 4, 5, 6],
+      [0, 2, 5],
+      [0, 1, 2, 3, 4, 5, 6],
+      [0, 1, 2, 3, 5, 6]
+    ]
+    [0 .. 9]
 
 originalString = "abcdefg"
 
-fromOriginal :: String -> Map String Int
-fromOriginal st = Map.mapKeys (sort . map (st !!)) original
+encodeString :: String -> Int
+encodeString = foldl' (\acc x -> acc `setBit` (ord x - ord 'A')) 0
 
-decode :: Map String Int -> [String] -> [Int]
-decode stringID numbers = map (stringID Map.!) numbers
+fromOriginal :: String -> IntMap Int
+-- fromOriginal st = traceShow ans ans
+fromOriginal st = ans
+  where
+    ans = IM.fromList $ map (first (encodeString . map (st !!))) original
+
+decode :: IntMap Int -> [String] -> [Int]
+decode stringID = map ((stringID IM.!) . encodeString)
 
 day8 :: IO ()
 day8 = do
@@ -43,7 +52,7 @@ day8 = do
       . lines
       <$> (getDataDir >>= readFile . (++ "/input/input8.txt"))
   let strings = map fromOriginal (permutations originalString)
-      stringIDs = map (mapFirst (\x -> fromJust (find (\y -> all (`Map.member` y) x) strings))) input
+      stringIDs = map (first (\x -> fromJust (find (\y -> all ((`IM.member` y) . encodeString) x) strings))) input
       decoded = map (uncurry decode) stringIDs
   putStrLn
     . ("day8a: " ++)
